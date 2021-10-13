eb51394ece209ddcf6c54866dbbfcb0c4fc4e64d
18ksc <18ksc@DESKTOP-6KSP7EE>
2021-10-13 17:16:35 -0500

    refactoring initial commit

---
+++ simple-refactoring-examples/src/p6/form_template_method1/refactored/A.java
@@ -0,0 +1,41 @@
+package p6.form_template_method1.refactored;
+
+public class A {
+   void sendMessage(Service svc) {
+      new MessageSendTask().common(svc);
+   }
+   void saveMessage(Service svc) {
+      new MessageSaveTask().common(svc);
+   }
+
+   void receiveMessage(Service svc) {
+      new MessageReceiveTask().common(svc);
+   }
+}
+abstract class Template {
+   void common(Service svc) {
+      svc.beginTask();
+      processMessage(svc);
+      svc.endTask();
+   }
+   abstract void processMessage(Service svc);
+}
+class MessageSendTask extends Template {
+   @Override
+   void processMessage(Service svc) {
+      svc.sendMessage();
+   }
+}
+class MessageSaveTask extends Template {
+   @Override
+   void processMessage(Service svc) {
+      svc.saveMessage();
+   }
+}
+class MessageReceiveTask extends Template {
+
+   @Override
+   void processMessage(Service svc) {
+      svc.receiveMessage();
+   }
+}
---
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,43 @@
+package q1.extract_method.refactored;
+
+import java.util.List;
+
+import q1.extract_method.refactored.Graph.Edge;
+import q1.extract_method.refactored.Graph.Node;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+	   extractedMethod(nodes, p);
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+	   extractedMethod(edgeList, p);
+
+      return null;
+   }
+   
+   <T extends Graph> void extractedMethod(List<T> objs, String p)
+   {
+	   for (T obj: objs)
+	   {
+		   if(obj.contains(p))
+			   System.out.println(obj);
+	   }
+   }
+}
+
+class Graph {
+   String name;
+
+   public boolean contains(String p) {
+      return name.contains(p);
+   }
+   
+class Node extends Graph {
+}
+
+class Edge extends Graph {
+}
+
+}
